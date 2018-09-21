a63e283addb88ebde28712dca7fc26aa6391b2c4
Drazen <Drazen@192.168.0.154>
2018-09-21 10:14:42 +0400

    21-Sep-18: Finish temperature conversion program

--- TempConvert/src/tempconvert/TempConvert.java
+++ TempConvert/src/tempconvert/TempConvert.java
@@ -5,6 +5,8 @@
  */
 package tempconvert;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Drazen
@@ -15,7 +17,14 @@ public class TempConvert {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double temp;
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Enter temperature in degrees Celsius:");
+        temp = keyboard.nextDouble();
+        
+        temp = (9.0 / 5.0) * temp + 32.0;
+        System.out.println("The temperature in degrees Fahrenheit is: " + temp);
     }
     
 }
